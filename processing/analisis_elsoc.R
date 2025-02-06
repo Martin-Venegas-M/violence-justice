8382a9161eae086bf97276a02ba0dd1c2fa9707c
Martin-Venegas-M <martin.venegas@ug.uchile.cl>
2025-02-06 14:13:26 -0300

    automatizar archivo riclpm models (basic)

--- processing/analisis_elsoc.R
+++ processing/analisis_elsoc.R
@@ -1,9 +1,8 @@
-
 # 0. Identification -------------------------------------------------------
 
-#Title: Analysis code for a research paper on Justice and violence
-#Institution: Centro de Estudios de Conflicto y Cohesión Social (COES)
-#Responsable: Researcher
+# Title: Analysis code for a research paper on Justice and violence
+# Institution: Centro de Estudios de Conflicto y Cohesión Social (COES)
+# Responsable: Researcher
 
 # Executive Summary: This script contains the code to create the analysis code for Justice and Violence
 # Date: January 30, 2025
@@ -12,14 +11,15 @@ rm(list = ls())
 
 # 1. Load packages --------------------------------------------------------
 
-if (!require("pacman")) install.packages("pacman")  #if pacman es missing, install
+if (!require("pacman")) install.packages("pacman") # if pacman es missing, install
 
 pacman::p_load(
   tidyverse,
   haven,
   tidylog,
-  lavaan
-  )
+  lavaan,
+  writexl
+)
 
 # 2. Load data and functions ----------------------------------------------
 
@@ -28,13 +28,47 @@ source("processing/func_sint.R")
 
 # 3. Estimate RICLPM ------------------------------------------------------
 
-fit_riclpm1 <- sem(
-  text_riclpm("brecha_perc", "f05_03", 7), 
-  data = elsoc, 
-  estimator = "MLR", 
-  missing = "FIML",
-  meanstructure = T,   
-  int.ov.free = T
+# Function for estimating multiple dependent variables for one dependent variable
+list_fits <- function(x, waves1 = c(1:7), waves2 = c(1:4, 6:7)) {
+  fits <- list(
+    fit1 = estimate_riclpm(text_riclpm(x, "f05_01", waves2)), # Info not available for w05
+    fit2 = estimate_riclpm(text_riclpm(x, "f05_02", waves2)), # Info not available for w05
+    fit3 = estimate_riclpm(text_riclpm(x, "f05_03", waves1)),
+    fit4 = estimate_riclpm(text_riclpm(x, "f05_04", waves1)),
+    fit5 = estimate_riclpm(text_riclpm(x, "f05_05", waves2)), # Info not available for w05
+    fit6 = estimate_riclpm(text_riclpm(x, "f05_06", waves1)),
+    fit7 = estimate_riclpm(text_riclpm(x, "f05_07", waves1)),
+    fit8 = estimate_riclpm(text_riclpm(x, "t06_01", c(1:4, 6))) # Info not available for w05 and w07
   )
 
-reg_sig(fit_riclpm1)
+  return(fits)
+}
+
+fits_brecha_perc <- list_fits("brecha_perc")
+fits_brecha_just <- list_fits("brecha_just")
+fits_c18_11 <- list_fits("c18_11")
+fits_d02_01 <- list_fits("d02_01", waves1 = c(1:4, 6:7))
+fits_d02_02 <- list_fits("d02_02", waves1 = c(1:4, 6:7))
+fits_d02_03 <- list_fits("d02_03", waves1 = c(1:4, 6:7))
+
+# 4. Create tabs for RICLPM -----------------------------------------------
+
+vector_vary <- c(paste0("f05_0", rep(1:7)), "t06_01")
+
+tab_brecha_perc <- bind_rows(map2(.x = fits_brecha_perc, .y = vector_vary, .f = ~ reg_sig(.x, "brecha_perc", .y)))
+tab_brecha_just <- bind_rows(map2(.x = fits_brecha_just, .y = vector_vary, .f = ~ reg_sig(.x, "brecha_just", .y)))
+tab_c18_11 <- bind_rows(map2(.x = fits_c18_11, .y = vector_vary, .f = ~ reg_sig(.x, "c18_11", .y)))
+tab_d02_01 <- bind_rows(map2(.x = fits_d02_01, .y = vector_vary, .f = ~ reg_sig(.x, "d02_01", .y)))
+tab_d02_02 <- bind_rows(map2(.x = fits_d02_02, .y = vector_vary, .f = ~ reg_sig(.x, "d02_02", .y)))
+tab_d02_03 <- bind_rows(map2(.x = fits_d02_03, .y = vector_vary, .f = ~ reg_sig(.x, "d02_03", .y)))
+
+# 5. Excel with estimations ----------------------------------------------
+
+write_xlsx(list(
+  "BRECHA PERCIBIDA" = tab_brecha_perc,
+  "BRECHA JUSTA" = tab_brecha_just,
+  "TOLERANCIA" = tab_c18_11,
+  "JUSTICIA PENSIONES" = tab_d02_01,
+  "JUSTICIA EDUCACION" = tab_d02_02,
+  "JUSTICIA SALUD" = tab_d02_03
+), "output/riclpm_violencie_justice.xlsx")
--- processing/func_sint.r
+++ processing/func_sint.r
@@ -1,59 +1,88 @@
-text_riclpm <- function(x, y, n_olas) {
-  # Crear los componentes between
-  RI_x <- paste0("RI_x =~ ", paste0("1*", x, "_w", sprintf("%02d", 1:n_olas), collapse = " + "))
-  RI_y <- paste0("RI_y =~ ", paste0("1*", y, "_w", sprintf("%02d", 1:n_olas), collapse = " + "))
-  
-  # Crear los componentes within
-  cx <- paste0("cx", 1:n_olas, " =~ 1*", x, "_w", sprintf("%02d", 1:n_olas), collapse = "\n")
-  cy <- paste0("cy", 1:n_olas, " =~ 1*", y, "_w", sprintf("%02d", 1:n_olas), collapse = "\n")
-  
-  # Constrenir las varianzas del error de medición a cero
-  var_x <- paste0(x, "_w", sprintf("%02d", 1:n_olas), " ~~ 0*", x, "_w", sprintf("%02d", 1:n_olas), collapse = "\n")
-  var_y <- paste0(y, "_w", sprintf("%02d", 1:n_olas), " ~~ 0*", y, "_w", sprintf("%02d", 1:n_olas), collapse = "\n")
-  
-  # Estimar los efectos lagged
-  lag_cx <- paste0("cx", 2:n_olas, " ~ cx", 1:(n_olas-1), " + cy", 1:(n_olas-1), collapse = "\n")
-  lag_cy <- paste0("cy", 2:n_olas, " ~ cx", 1:(n_olas-1), " + cy", 1:(n_olas-1), collapse = "\n")
-  
-  # Estimar la covarianza entre los componentes within t=1
-  cov_t1 <- "cx1 ~~ cy1"
-  
-  # Estimar las covarianzas entre los residuos del componente within (innovations)
-  cov_resid <- paste0("cx", 2:n_olas, " ~~ cy", 2:n_olas, collapse = "\n")
-  
-  # Estimar la varianza y covarianza entre los RI
+# Function for creating text object for lavaan ----------------------------------------------
+text_riclpm <- function(x, y, waves) {
+  # Create between components
+  RI_x <- paste0("RI_x =~ ", paste0("1*", x, "_w", sprintf("%02d", waves), collapse = " + "))
+  RI_y <- paste0("RI_y =~ ", paste0("1*", y, "_w", sprintf("%02d", waves), collapse = " + "))
+
+  # Create within components
+  cx <- paste0("cx", waves, " =~ 1*", x, "_w", sprintf("%02d", waves), collapse = "\n")
+  cy <- paste0("cy", waves, " =~ 1*", y, "_w", sprintf("%02d", waves), collapse = "\n")
+
+  # Constrain measurement error variances to cero
+  var_x <- paste0(x, "_w", sprintf("%02d", waves), " ~~ 0*", x, "_w", sprintf("%02d", waves), collapse = "\n")
+  var_y <- paste0(y, "_w", sprintf("%02d", waves), " ~~ 0*", y, "_w", sprintf("%02d", waves), collapse = "\n")
+
+  # Estimate lagged efects only for consecutive waves in the vector
+  lag_cx <- paste0("cx", waves[-1], " ~ cx", waves[-length(waves)], " + cy", waves[-length(waves)], collapse = "\n")
+  lag_cy <- paste0("cy", waves[-1], " ~ cx", waves[-length(waves)], " + cy", waves[-length(waves)], collapse = "\n")
+
+  # Estimate covariance bewteen within components t = 1
+  cov_t1 <- paste0("cx", waves[1], " ~~ cy", waves[1])
+
+  # Estimate covariances between within residuals of within components
+  cov_resid <- paste0("cx", waves[-1], " ~~ cy", waves[-1], collapse = "\n")
+
+  # Estimate variance and covariance between RI
   cov_RI <- "RI_x ~~ RI_x\nRI_y ~~ RI_y\nRI_x ~~ RI_y"
-  
-  # Fijar la correlación entre los RI y componentes within t=1 a cero
-  fix_RI <- "RI_x ~~ 0*cx1\nRI_x ~~ 0*cy1\nRI_y ~~ 0*cx1\nRI_y ~~ 0*cy1"
-  
-  # Combinar todo el modelo
+
+  # Fix correlation bewteen RI and within components t = 1 to cero
+  fix_RI <- paste0(
+    "RI_x ~~ 0*cx", waves[1], "\nRI_x ~~ 0*cy", waves[1],
+    "\nRI_y ~~ 0*cx", waves[1], "\nRI_y ~~ 0*cy", waves[1]
+  )
+
+  # Combine model
   modelo <- paste(RI_x, RI_y, cx, cy, var_x, var_y, lag_cx, lag_cy, cov_t1, cov_resid, cov_RI, fix_RI, sep = "\n\n")
-  
+
   return(modelo)
 }
 
-# Ejemplo de uso
-# modelo_riclpm <- generar_riclpm("brecha_perc", "f05_03", 7)
+# Example
+# test_riclpm <- text_riclpm("brecha_perc", "f05_03", c(1:7))
 # cat(modelo_riclpm)
 
-reg_sig <- function(fit) {
+# test <- sem(
+#   test_riclpm,
+#   data = elsoc,
+#   estimator = "MLR",
+#   missing = "FIML",
+#   meanstructure = T,
+#   int.ov.free = T
+# )
 
+# summary(test)
+
+# Function for creating a table with significative regression effects -----------------------------------
+reg_sig <- function(fit, x, y) {
   parameters <- parameterEstimates(fit, standardized = TRUE) %>%
     filter(op == "~", pvalue < 0.05) %>%
     select(lhs, op, rhs, est, se, z, pvalue, std.all) %>%
     arrange(pvalue)
 
-  fitmeasures <- fitMeasures(fit_riclpm1)
-  
+  fitmeasures <- fitMeasures(fit)
+
   parameters <- parameters %>% mutate(
     CFI = fitmeasures[["cfi"]],
-    RMSEA = fitmeasures[["rmsea"]]
-  )
+    RMSEA = fitmeasures[["rmsea"]],
+    varx := x, 
+    vary := y 
+  ) %>% relocate(vary, varx, everything())
 
   return(parameters)
 }
 
-# Uso:
-# tabla_significativas <- obtener_regresiones_significativas(fit_riclpm_f05_03)
-# print(tabla_significativas)
+# Example:
+# tab_sig <- reg_sig(fit_riclpm_f05_03)
+# print(tab_sig)
+
+# Function for estimating RICLPM Model. It assumes data as "elsoc". ------------------------------------
+estimate_riclpm <- function(text_object) {
+  sem(
+    text_object,
+    data = elsoc,
+    estimator = "MLR",
+    missing = "FIML",
+    meanstructure = T,
+    int.ov.free = T
+  )
+}
