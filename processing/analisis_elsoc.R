83e172ec90e3135375d5f6089b324a940e56fa8a
Martin-Venegas-M <martin.venegas@ug.uchile.cl>
2025-07-18 00:24:46 -0400

    add moderation ideol2

--- processing/analisis_elsoc.R
+++ processing/analisis_elsoc.R
@@ -112,7 +112,8 @@ cor.test(elsoc$brecha_perc_w07, elsoc$c18_11_w07)
 
 # writeLines(text_riclpm_v2("brecha_perc", "f05_04", c(1:7), inv_preds = c("m0_edad_w01", "m0_sexo_w01", "m01_w01"), constrain = TRUE))
 
-text_moderation <- "
+# 7.1 Moderación con cuatro grupos ---------------------------------------
+text_moderation4 <- "
 
 RI_x =~ 1*brecha_perc_w01 + 1*brecha_perc_w02 + 1*brecha_perc_w03 + 1*brecha_perc_w04 + 1*brecha_perc_w05 + 1*brecha_perc_w06 + 1*brecha_perc_w07
 RI_y =~ 1*f05_04_w01 + 1*f05_04_w02 + 1*f05_04_w03 + 1*f05_04_w04 + 1*f05_04_w05 + 1*f05_04_w06 + 1*f05_04_w07
@@ -172,6 +173,70 @@ RI_y ~~ 0*cx1
 RI_y ~~ 0*cy1
 
 "
-fit_moderation <- estimate_riclpm(text_moderation, g = "ideol")
-summary(fit_moderation, fit.measures = TRUE)
+fit_moderation4 <- estimate_riclpm(text_moderation4, g = "ideol4")
+summary(fit_moderation4, fit.measures = TRUE)
 #! OJO! Con cuatro grupos se pierde el efecto. Probar otros grupos.
+
+# 7.2 Moderación con dos grupos ------------------------------------------
+text_moderation2 <- "
+
+RI_x =~ 1*brecha_perc_w01 + 1*brecha_perc_w02 + 1*brecha_perc_w03 + 1*brecha_perc_w04 + 1*brecha_perc_w05 + 1*brecha_perc_w06 + 1*brecha_perc_w07
+RI_y =~ 1*f05_04_w01 + 1*f05_04_w02 + 1*f05_04_w03 + 1*f05_04_w04 + 1*f05_04_w05 + 1*f05_04_w06 + 1*f05_04_w07
+cx1 =~ 1*brecha_perc_w01
+cx2 =~ 1*brecha_perc_w02
+cx3 =~ 1*brecha_perc_w03
+cx4 =~ 1*brecha_perc_w04
+cx5 =~ 1*brecha_perc_w05
+cx6 =~ 1*brecha_perc_w06
+cx7 =~ 1*brecha_perc_w07
+cy1 =~ 1*f05_04_w01
+cy2 =~ 1*f05_04_w02
+cy3 =~ 1*f05_04_w03
+cy4 =~ 1*f05_04_w04
+cy5 =~ 1*f05_04_w05
+cy6 =~ 1*f05_04_w06
+cy7 =~ 1*f05_04_w07
+brecha_perc_w01 ~~ 0*brecha_perc_w01
+brecha_perc_w02 ~~ 0*brecha_perc_w02
+brecha_perc_w03 ~~ 0*brecha_perc_w03
+brecha_perc_w04 ~~ 0*brecha_perc_w04
+brecha_perc_w05 ~~ 0*brecha_perc_w05
+brecha_perc_w06 ~~ 0*brecha_perc_w06
+brecha_perc_w07 ~~ 0*brecha_perc_w07
+f05_04_w01 ~~ 0*f05_04_w01
+f05_04_w02 ~~ 0*f05_04_w02
+f05_04_w03 ~~ 0*f05_04_w03
+f05_04_w04 ~~ 0*f05_04_w04
+f05_04_w05 ~~ 0*f05_04_w05
+f05_04_w06 ~~ 0*f05_04_w06
+f05_04_w07 ~~ 0*f05_04_w07
+cx2 ~ c(a1, a2)*cx1 + c(b1, b2)*cy1 + c(c1, c2)*m0_edad_w01 + c(d1, d2)*m0_sexo_w01 + c(e1, e2)*m01_w01
+cx3 ~ c(a1, a2)*cx2 + c(b1, b2)*cy2 + c(c1, c2)*m0_edad_w01 + c(d1, d2)*m0_sexo_w01 + c(e1, e2)*m01_w01
+cx4 ~ c(a1, a2)*cx3 + c(b1, b2)*cy3 + c(c1, c2)*m0_edad_w01 + c(d1, d2)*m0_sexo_w01 + c(e1, e2)*m01_w01
+cx5 ~ c(a1, a2)*cx4 + c(b1, b2)*cy4 + c(c1, c2)*m0_edad_w01 + c(d1, d2)*m0_sexo_w01 + c(e1, e2)*m01_w01
+cx6 ~ c(a1, a2)*cx5 + c(b1, b2)*cy5 + c(c1, c2)*m0_edad_w01 + c(d1, d2)*m0_sexo_w01 + c(e1, e2)*m01_w01
+cx7 ~ c(a1, a2)*cx6 + c(b1, b2)*cy6 + c(c1, c2)*m0_edad_w01 + c(d1, d2)*m0_sexo_w01 + c(e1, e2)*m01_w01
+cy2 ~ c(f1, f2)*cx1 + c(g1, g2)*cy1 + c(h1, h2)*m0_edad_w01 + c(i1, i2)*m0_sexo_w01 + c(j1, j2)*m01_w01
+cy3 ~ c(f1, f2)*cx2 + c(g1, g2)*cy2 + c(h1, h2)*m0_edad_w01 + c(i1, i2)*m0_sexo_w01 + c(j1, j2)*m01_w01
+cy4 ~ c(f1, f2)*cx3 + c(g1, g2)*cy3 + c(h1, h2)*m0_edad_w01 + c(i1, i2)*m0_sexo_w01 + c(j1, j2)*m01_w01
+cy5 ~ c(f1, f2)*cx4 + c(g1, g2)*cy4 + c(h1, h2)*m0_edad_w01 + c(i1, i2)*m0_sexo_w01 + c(j1, j2)*m01_w01
+cy6 ~ c(f1, f2)*cx5 + c(g1, g2)*cy5 + c(h1, h2)*m0_edad_w01 + c(i1, i2)*m0_sexo_w01 + c(j1, j2)*m01_w01
+cy7 ~ c(f1, f2)*cx6 + c(g1, g2)*cy6 + c(h1, h2)*m0_edad_w01 + c(i1, i2)*m0_sexo_w01 + c(j1, j2)*m01_w01
+cx1 ~~ cy1
+cx2 ~~ cy2
+cx3 ~~ cy3
+cx4 ~~ cy4
+cx5 ~~ cy5
+cx6 ~~ cy6
+cx7 ~~ cy7
+RI_x ~~ RI_x
+RI_y ~~ RI_y
+RI_x ~~ RI_y
+RI_x ~~ 0*cx1
+RI_x ~~ 0*cy1
+RI_y ~~ 0*cx1
+RI_y ~~ 0*cy1
+
+"
+fit_moderation2 <- estimate_riclpm(text_moderation2, g = "ideol2")
+summary(fit_moderation2, fit.measures = TRUE)
--- processing/proc_elsoc.R
+++ processing/proc_elsoc.R
@@ -1,9 +1,8 @@
-
 # 0. Identification -------------------------------------------------------
 
-#Title: Processing code for a research paper on Justice and violence
-#Institution: Centro de Estudios de Conflicto y Cohesión Social (COES)
-#Responsable: Researcher
+# Title: Processing code for a research paper on Justice and violence
+# Institution: Centro de Estudios de Conflicto y Cohesión Social (COES)
+# Responsable: Researcher
 
 # Executive Summary: This script contains the code to create the database needed to elaborate the analyses on Justice and Violence
 # Date: January 28, 2025
@@ -12,13 +11,13 @@ rm(list = ls())
 
 # 1. Load packages --------------------------------------------------------
 
-if (!require("pacman")) install.packages("pacman")  #if pacman es missing, install
+if (!require("pacman")) install.packages("pacman") # if pacman es missing, install
 
 pacman::p_load(
   tidyverse,
   haven,
   tidylog
-  )
+)
 
 # 2. Load data ------------------------------------------------------------
 
@@ -26,52 +25,59 @@ load(url("https://dataverse.harvard.edu/api/access/datafile/10797986"))
 
 # 3. Select variables -----------------------------------------------------
 
-elsoc <- elsoc_wide_2016_2023 %>% dplyr::select(
-  idencuesta,
-  tipo_atricion,
-  tipo_caso,
-  starts_with("m0_edad"),
-  starts_with("m0_sexo"),
-  starts_with("m01_"),
-  starts_with("c15"),
-  contains("f05_"),
-  contains("f06_"),
-  contains("t06_01"),
-  contains("t09"),
-  contains("d03_01"),
-  contains("d03_02"),
-  contains("d04_01"),
-  contains("d04_02"),
-  contains("c18_11"),
-  contains("d02_")
-  ) %>% 
+elsoc <- elsoc_wide_2016_2023 %>%
+  dplyr::select(
+    idencuesta,
+    tipo_atricion,
+    tipo_caso,
+    starts_with("m0_edad"),
+    starts_with("m0_sexo"),
+    starts_with("m01_"),
+    starts_with("c15"),
+    contains("f05_"),
+    contains("f06_"),
+    contains("t06_01"),
+    contains("t09"),
+    contains("d03_01"),
+    contains("d03_02"),
+    contains("d04_01"),
+    contains("d04_02"),
+    contains("c18_11"),
+    contains("d02_")
+  ) %>%
   mutate(
-    brecha_perc_w01 = log(d03_01_w01/d03_02_w01),
-    brecha_perc_w02 = log(d03_01_w01/d03_02_w02),
-    brecha_perc_w03 = log(d03_01_w01/d03_02_w03),
-    brecha_perc_w04 = log(d03_01_w01/d03_02_w04),
-    brecha_perc_w05 = log(d03_01_w01/d03_02_w05),
-    brecha_perc_w06 = log(d03_01_w01/d03_02_w06),
-    brecha_perc_w07 = log(d03_01_w01/d03_02_w07)
-  ) %>% 
+    brecha_perc_w01 = log(d03_01_w01 / d03_02_w01),
+    brecha_perc_w02 = log(d03_01_w01 / d03_02_w02),
+    brecha_perc_w03 = log(d03_01_w01 / d03_02_w03),
+    brecha_perc_w04 = log(d03_01_w01 / d03_02_w04),
+    brecha_perc_w05 = log(d03_01_w01 / d03_02_w05),
+    brecha_perc_w06 = log(d03_01_w01 / d03_02_w06),
+    brecha_perc_w07 = log(d03_01_w01 / d03_02_w07)
+  ) %>%
   mutate(
-    brecha_just_w01 = log(d04_01_w01/d04_02_w01),
-    brecha_just_w02 = log(d04_01_w01/d04_02_w02),
-    brecha_just_w03 = log(d04_01_w01/d04_02_w03),
-    brecha_just_w04 = log(d04_01_w01/d04_02_w04),
-    brecha_just_w05 = log(d04_01_w01/d04_02_w05),
-    brecha_just_w06 = log(d04_01_w01/d04_02_w06),
-    brecha_just_w07 = log(d04_01_w01/d04_02_w07)
-  ) %>% select(
+    brecha_just_w01 = log(d04_01_w01 / d04_02_w01),
+    brecha_just_w02 = log(d04_01_w01 / d04_02_w02),
+    brecha_just_w03 = log(d04_01_w01 / d04_02_w03),
+    brecha_just_w04 = log(d04_01_w01 / d04_02_w04),
+    brecha_just_w05 = log(d04_01_w01 / d04_02_w05),
+    brecha_just_w06 = log(d04_01_w01 / d04_02_w06),
+    brecha_just_w07 = log(d04_01_w01 / d04_02_w07)
+  ) %>%
+  select(
     -contains("d03"),
     -contains("d04")
-  ) %>% mutate(
+  ) %>%
+  mutate(
     across(everything(), ~ if_else(. %in% c(-999, -888, -777, -666, -Inf, Inf), NA, .)),
-        ideol = case_when(
+    ideol4 = case_when(
       c15_w01 %in% c(0:3) ~ 1, # Izquierda
       c15_w01 %in% c(4:6) ~ 2, # Centro
       c15_w01 %in% c(7:10) ~ 3, # Derecho
       c15_w01 %in% c(11, 12) ~ 4 # Ninguno Ns/NR
+    ),
+    ideol2 = case_when(
+      c15_w01 %in% c(0:10) ~ 1, # Tiene tendencia
+      c15_w01 %in% c(11, 12) ~ 0 # No declara tendencia
     ),
     across(c(paste0("c15_w0", rep(1:7))), ~ if_else(. %in% c(11, 12), NA, .))
   )
@@ -79,4 +85,3 @@ elsoc <- elsoc_wide_2016_2023 %>% dplyr::select(
 # 4. Save data ------------------------------------------------------------
 
 saveRDS(elsoc, "input/data/proc_elsoc.RDS")
-
